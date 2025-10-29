625e1ae0e6eebc195ab3835366060e150f69099e
Hazel Ngo <hngo001@gold.ac.uk>
2025-10-29 18:30:41 +0000

    Initial Version A-E

---
+++ routes/main.js
@@ -0,0 +1,36 @@
+// Create a new router
+const express = require("express");
+const router = express.Router();
+// Define our data
+var shopData = {shopName: "The Thirsty Student",
+    productCategories: ["Beer", "Wine", "Soft Drink", "Hot Drink"]
+};
+
+// Handle the main routes
+router.get("/", (req, res) => {
+    res.render("index.ejs", shopData)
+}); 
+
+router.get("/about", (req, res) => {
+    res.render("about.ejs", shopData)
+});
+ router.get("/search", (req,res) => {
+    res.render("search.ejs", shopData)
+ });
+ router.get('/search_result', function (req, res) {
+    // TODO: search in the database
+    res.send("You searched for " + req.query.search_text + " in " + req.query.category);
+ });
+
+ router.get("/register", (req,res) => {
+    res.render("register.ejs",  shopData); 
+});
+ 
+router.post("/registered", (req,res) => { 
+    res.send(' Hello '+ req.body.first + ' '+ req.body.last +' you are now registered!. We will send an email to you at ' + req.body.email);    
+});
+
+// TODO
+
+// Export the router object so index.js can access it
+module.exports = router;
